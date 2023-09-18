8a27405d32f8f6dbb05aed79f95e736bb5ea39e9
Rizal Alfandi <rizalalfandi123456789@gmail.com>
2023-09-18 13:50:00 +0700

    edit readme.md

--- src/lib/default-headers.ts
+++ src/lib/default-headers.ts
@@ -1,9 +1,6 @@
 const headers = new Headers();
 
-headers.set(
-  "Authorization",
-  "Bearer bd1fcd1ee5037a6ec1505ec25f5054996802a31a9b12a9232b4be87ba593bac9"
-);
+headers.set("Authorization", process.env.AUTH_TOKEN!);
 
 headers.set("Content-Type", "application/json");
 
